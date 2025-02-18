246da59a3e015e6a06b95bf62c37b45e59e01115
Kholzt <99956928+Kholzt@users.noreply.github.com>
2025-02-18 18:51:48 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,21 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+            <div class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 ">
+                    <h6>Buku</h6>
+                    <h1 class="text-4xl">20</h1>
+                    <p class="text-gray-600 text-sm">Semua Buku yang tercatat di sistem</p>
+                </div>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 ">
+                    <h6>Anggota</h6>
+                    <h1 class="text-4xl">20</h1>
+                    <p class="text-gray-600 text-sm">Semua Anggota yang tercatat di sistem</p>
+                </div>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 ">
+                    <h6>Petugas</h6>
+                    <h1 class="text-4xl">20</h1>
+                    <p class="text-gray-600 text-sm">Semua Petugas yang tercatat di sistem</p>
                 </div>
             </div>
         </div>
